aea44b340cd4c7f1ac2c937c3db94cbb76054f7f
Clarence Mills <cmills@opensitesolutions.com>
2016-05-12 13:10:47 -0400

    default template for creating new files, include include files located within the includes directory

---
+++ template_include.php
@@ -0,0 +1,27 @@
+<?php
+set_include_path('includes:../includes');
+ ?>
+
+ <?php
+ include 'navbar.inc'
+?>
+
+ <?php
+include 'header.inc';
+?>
+
+<?php
+include 'left.inc';
+?>
+
+<?php
+include 'center.inc';
+?>
+
+<?php
+include 'right.inc';
+?>
+
+<?php
+include 'footer.inc';
+ ?>
