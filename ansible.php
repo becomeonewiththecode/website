976a4a1ed8fa1ee6786d82dd0ccc138f29061697
Clarence Mills <cmills@opensitesolutions.com>
2016-05-12 18:23:01 -0400

    New file ansible.php

---
+++ ansible.php
@@ -0,0 +1,56 @@
+<?php
+set_include_path('includes:../includes');
+ ?>
+
+ <?php
+ include 'navbar.inc'
+?>
+
+ <?php
+include 'header.inc';
+?>
+
+<?php
+include 'left.inc';
+?>
+
+<!--- begin center column Place information here--->
+<div class="col-md-8">
+  <div class="container-fluid">
+    <!--- begin content --->
+    <div class="page-header">
+            <h2> Ansible</h2>
+          </div>
+
+          <div class="jumbotron" class="">
+            <p class="text-muted">
+              Automate infrastucture, app deployment and configuration management with ease.
+            </p>
+          </div>
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title"><span class="glyphicon glyphicon-cog"></span> Get started with Ansible</h3>
+          </div>
+          <div class="panel-body">
+              <a href="#" class="list-group-item disabled"> Configuration files</a>
+              <a href="#" class="list-group-item disabled"> Command line</a>
+              <a href="#" class="list-group-item disabled"> Playbooks</a>
+              <a href="#" class="list-group-item disabled"> Roles</a>
+          </div>
+        </div> 
+
+    <!--- end content --->
+
+
+  </div>
+</div>
+<!--- end center column --->
+
+<?php
+include 'right.inc';
+?>
+
+<?php
+include 'footer.inc';
+ ?>
