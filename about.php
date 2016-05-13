b8f6c97959dc85586dd9e68872e9e384f561a941
Clarence Mills <cmills@opensitesolutions.com>
2016-05-13 18:50:25 -0400

    New file about.php

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+set_include_path('includes:../includes');
+ ?>
+
+ <?php
+ include 'navbar.inc'
+?>
+
+ <?php
+include 'header.inc';
+?>
+
+<?php
+include 'left.inc';
+?>
+
+<!--- begin center column Place information here--->
+<div class="col-md-8">
+  <div class="container-fluid">
+    <!--- begin content --->
+
+
+    <!--- end content --->
+
+  </div>
+</div>
+<!--- end center column --->
+
+<?php
+include 'right.inc';
+?>
+
+<?php
+include 'footer.inc';
+ ?>
