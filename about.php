9cd638699c49540f92f20426546b29fefa513c6d
Clarence Mills <cmills@opensitesolutions.com>
2016-05-13 20:09:14 -0400

    Describe the purpse of the site

--- about.php
+++ about.php
@@ -18,7 +18,15 @@ include 'left.inc';
 <div class="col-md-8">
   <div class="container-fluid">
     <!--- begin content --->
+    <div class="page-header">
+      <h2> About</h2>
+    </div>
 
+    <div>
+      System administration has changed to the point where visting a datacenter visit or physically logging into a system to correct a problem is not longer needed. It's at a point where code can be used to fix or mitigate errors and problems without requiring physical intervention.  Code can now build, deploy, automate, discover, monitor and recover serives. System administration is now about using code to provide highly available, robust, resilent platforms and applications that can be deployed and maintained with greater ease.
+
+      This site will shine light on how code can accomplish those goals.
+    </div>
 
     <!--- end content --->
 
